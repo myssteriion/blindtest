c89f2603eba6bc475a258fd86dabf90d3f05eeaa
Myssteriion <myssteriion@hotmail.com>
2019-06-16 18:33:08 +0200

    feat(MusicDAO) : add dao + test

---
+++ src/main/java/com/myssteriion/blindtest/db/dao/MusicDAO.java
@@ -0,0 +1,119 @@
+package com.myssteriion.blindtest.db.dao;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.myssteriion.blindtest.db.AbstractDAO;
+import com.myssteriion.blindtest.db.exception.EntityManagerException;
+import com.myssteriion.blindtest.model.music.MusicDTO;
+import com.myssteriion.blindtest.model.music.Theme;
+import com.myssteriion.blindtest.tools.Tool;
+
+@Component
+public class MusicDAO extends AbstractDAO<MusicDTO> {
+
+	@Override
+	public MusicDTO find(MusicDTO dto) throws EntityManagerException {
+		
+		Tool.verifyValue("dto", dto);
+		
+		try ( Statement statement = em.createStatement() ) {
+			
+			MusicDTO dtoToReturn = null;
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("SELECT * FROM musique ");
+			sb.append("WHERE name = '" + dto.getName() + "' AND theme = '" + dto.getTheme() + "'");
+			
+			ResultSet rs = statement.executeQuery( sb.toString() );
+			while ( rs.next() ) {
+				
+				dtoToReturn = new MusicDTO( rs.getString("name"), Theme.valueOf(rs.getString("theme")), rs.getInt("nbPlayed") );
+				dtoToReturn.setId( rs.getString("id") );
+			}
+
+			return dtoToReturn;
+		}
+		catch (Exception e) {
+			throw new EntityManagerException("Can't find dto.", e);
+		}
+	}
+	
+	@Override
+	public List<MusicDTO> findAll() throws EntityManagerException {
+		
+		try ( Statement statement = em.createStatement() ) {
+			
+			List<MusicDTO> dtoList = new ArrayList<>();
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("SELECT * FROM musique");
+			
+			ResultSet rs = statement.executeQuery( sb.toString() );
+			while ( rs.next() ) {
+				
+				MusicDTO dto = new MusicDTO( rs.getString("name"), Theme.valueOf(rs.getString("theme")), rs.getInt("nbPlayed") );
+				dto.setId( rs.getString("id") );
+				dtoList.add(dto);
+			}
+			
+			return dtoList;
+		}
+		catch (Exception e) {
+			throw new EntityManagerException("Can't find all dto.", e);
+		}
+	}
+	
+	@Override
+	public MusicDTO saveOrUpdate(MusicDTO dto) throws EntityManagerException {
+		
+		Tool.verifyValue("dto", dto);
+
+		MusicDTO dtoFinded = find(dto);
+		MusicDTO dtoSaved = (dtoFinded == null) ? save(dto) : update(dtoFinded);
+		
+		return dtoSaved;
+	}
+	
+	
+	
+	private MusicDTO save(MusicDTO dto) throws EntityManagerException {
+		
+		try ( Statement statement = em.createStatement() ) {
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("INSERT INTO musique(name, theme, nbPlayed) ");
+			sb.append("VALUES ('" + dto.getName() + "', '" + dto.getTheme() + "', " + dto.getNbPlayed() + ")");
+			
+			statement.execute( sb.toString() );
+			return find(dto);
+		}
+		catch (Exception e) {
+			throw new EntityManagerException("Can't save dto.", e);
+		}
+	}
+	
+	private MusicDTO update(MusicDTO dto) throws EntityManagerException {
+		
+		Tool.verifyValue("dto -> id", dto.getId());
+		
+		try ( Statement statement = em.createStatement() ) {
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("UPDATE musique ");
+			sb.append("SET nbPlayed = " + dto.getNbPlayed() + " ");
+			sb.append("WHERE id = " + dto.getId());
+			
+			statement.execute( sb.toString() );
+			return dto;
+		}
+		catch (Exception e) {
+			throw new EntityManagerException("Can't update dto.", e);
+		}
+	}
+
+}
---
+++ src/test/java/com/myssteriion/blindtest/db/dao/MusicDAOTest.java
@@ -0,0 +1,165 @@
+package com.myssteriion.blindtest.db.dao;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+
+import org.h2.tools.SimpleResultSet;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.myssteriion.blindtest.AbstractTest;
+import com.myssteriion.blindtest.db.EntityManger;
+import com.myssteriion.blindtest.db.exception.EntityManagerException;
+import com.myssteriion.blindtest.model.music.MusicDTO;
+import com.myssteriion.blindtest.model.music.Theme;
+
+
+public class MusicDAOTest extends AbstractTest {
+
+	@Mock
+	protected EntityManger em;
+	
+	@InjectMocks
+	private MusicDAO dao;
+	
+	
+	
+	@Test
+	public void find() throws SQLException, EntityManagerException {
+
+		SimpleResultSet rsEmpty = getResultSet();
+		
+		SimpleResultSet rs = getResultSet();
+		rs.addRow("1", "name", Theme.ANNEES_80, 3);
+		
+		Statement statement = Mockito.mock(Statement.class);
+		Mockito.when(statement.executeQuery(Mockito.anyString())).thenReturn(rsEmpty, rs);
+		
+		EntityManagerException eme = new EntityManagerException("fake");
+		Mockito.when(em.createStatement()).thenThrow(eme).thenReturn(statement);
+		
+		
+		try {
+			dao.find(null);
+			Assert.fail("Doit lever une IllegalArgumentException car un param est KO.");
+		}
+		catch (IllegalArgumentException e) {
+			verifyException(new IllegalArgumentException("Le champ 'dto' est obligatoire."), e);
+		}
+		
+		
+		MusicDTO dto = new MusicDTO("name", Theme.ANNEES_80);
+		try {
+			dao.find(dto);
+			Assert.fail("Doit lever une EntityManagerException car le mock throw.");
+		}
+		catch (EntityManagerException e) {
+			verifyException(new EntityManagerException("fake"), eme);
+		}
+		
+		MusicDTO emDto = dao.find(dto);
+		Assert.assertNull(emDto);
+		
+		dto = new MusicDTO("name", Theme.ANNEES_80);
+		emDto = dao.find(dto);
+		Assert.assertNotNull(emDto);
+	}
+	
+	@Test
+	public void findAll() throws EntityManagerException, SQLException {
+
+		SimpleResultSet rs = getResultSet();
+		rs.addRow("1", "name", Theme.ANNEES_80, 3);
+		
+		Statement statement = Mockito.mock(Statement.class);
+		Mockito.when(statement.executeQuery(Mockito.anyString())).thenReturn(rs);
+		
+		EntityManagerException eme = new EntityManagerException("fake");
+		Mockito.when(em.createStatement()).thenThrow(eme).thenReturn(statement);
+		
+		
+		try {
+			dao.findAll();
+			Assert.fail("Doit lever une EntityManagerException car le mock throw.");
+		}
+		catch (EntityManagerException e) {
+			verifyException(new EntityManagerException("fake"), eme);
+		}
+		
+		MusicDTO dto = dao.findAll().get(0);
+		Assert.assertEquals( "1", dto.getId() );
+		Assert.assertEquals( "name", dto.getName() );
+		Assert.assertEquals( Theme.ANNEES_80, dto.getTheme() );
+		Assert.assertEquals( 3, dto.getNbPlayed() );
+	}
+
+	@Test
+	public void saveOrUpdate() throws EntityManagerException, SQLException {
+
+		dao = Mockito.spy( new MusicDAO() {{ em = MusicDAOTest.this.em; }} );
+		MockitoAnnotations.initMocks(dao);
+		
+		MusicDTO dto = new MusicDTO("name", Theme.ANNEES_80);
+		dto.setId("1");
+		Mockito.doReturn(null).doReturn(null).doReturn(dto).when(dao).find(Mockito.any(MusicDTO.class));
+		
+		Statement statement = Mockito.mock(Statement.class);
+		Mockito.when(statement.execute(Mockito.anyString())).thenReturn(true);
+		
+		EntityManagerException eme = new EntityManagerException("fake");
+		Mockito.when(em.createStatement()).thenThrow(eme).thenReturn(statement).thenThrow(eme).thenReturn(statement);
+		
+		
+		try {
+			dao.saveOrUpdate(null);
+			Assert.fail("Doit lever une IllegalArgumentException car un param est KO.");
+		}
+		catch (IllegalArgumentException e) {
+			verifyException(new IllegalArgumentException("Le champ 'dto' est obligatoire."), e);
+		}
+		
+		
+		MusicDTO dtoToSave = new MusicDTO("name", Theme.ANNEES_80);
+		try {
+			dao.saveOrUpdate(dtoToSave);
+			Assert.fail("Doit lever une EntityManagerException car le mock throw.");
+		}
+		catch (EntityManagerException e) {
+			verifyException(new EntityManagerException("Can't save dto.", eme), e);
+		}
+		
+		MusicDTO dtoReturned = dao.saveOrUpdate(dtoToSave);
+		Assert.assertEquals(dtoReturned.getId(), "1");
+		
+		
+		dtoToSave.setId("1");
+		try {
+			dao.saveOrUpdate(dtoToSave);
+			Assert.fail("Doit lever une EntityManagerException car le mock throw.");
+		}
+		catch (EntityManagerException e) {
+			verifyException(new EntityManagerException("Can't update dto.", eme), e);
+		}
+		
+		dtoReturned = dao.saveOrUpdate(dtoToSave);
+		Assert.assertEquals(dtoReturned.getId(), "1");
+	}
+	
+	
+	
+	private SimpleResultSet getResultSet() {
+		
+		SimpleResultSet rs = new SimpleResultSet();
+		rs.addColumn("id", Types.BIGINT, 100, 0);
+		rs.addColumn("name", Types.VARCHAR, 255, 0);
+		rs.addColumn("theme", Types.VARCHAR, 255, 0);
+		rs.addColumn("nbPlayed", Types.INTEGER, 100, 0);
+		return rs;
+	}
+	
+}
