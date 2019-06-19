c586037b8394f8b1b83b280bb9c807ec9c7a9816
Myssteriion <myssteriion@hotmail.com>
2019-06-19 22:22:06 +0200

    feat(Profil) : add DAO

---
+++ src/main/java/com/myssteriion/blindtest/db/dao/ProfilDAO.java
@@ -0,0 +1,140 @@
+package com.myssteriion.blindtest.db.dao;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.myssteriion.blindtest.db.AbstractDAO;
+import com.myssteriion.blindtest.db.exception.EntityManagerException;
+import com.myssteriion.blindtest.model.dto.ProfilDTO;
+import com.myssteriion.blindtest.tools.Tool;
+
+@Component
+public class ProfilDAO extends AbstractDAO<ProfilDTO> {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProfilDAO.class);
+	
+	
+	
+	@Override
+	public ProfilDTO find(ProfilDTO dto) throws EntityManagerException {
+		
+		Tool.verifyValue("dto", dto);
+		
+		try ( Statement statement = em.createStatement() ) {
+			
+			ProfilDTO dtoToReturn = null;
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("SELECT * FROM profil ");
+			sb.append("WHERE name = '" + dto.getName() + "'");
+			
+			ResultSet rs = statement.executeQuery( sb.toString() );
+			while ( rs.next() ) {
+				
+				dtoToReturn = new ProfilDTO( rs.getString("name"), rs.getString("avatar"), rs.getInt("playedGames"), rs.getInt("listenedMusics"), rs.getInt("foundMusics") );
+				dtoToReturn.setId( rs.getString("id") );
+			}
+
+			return dtoToReturn;
+		}
+		catch (Exception e) {
+			
+			String message = "Can't find dto.";
+			LOGGER.error(message, e);
+			throw new EntityManagerException(message, e);
+		}
+	}
+	
+	@Override
+	public List<ProfilDTO> findAll() throws EntityManagerException {
+		
+		try ( Statement statement = em.createStatement() ) {
+			
+			List<ProfilDTO> dtoList = new ArrayList<>();
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("SELECT * FROM music");
+			
+			ResultSet rs = statement.executeQuery( sb.toString() );
+			while ( rs.next() ) {
+				
+				ProfilDTO dto = new ProfilDTO( rs.getString("name"), rs.getString("avatar"), rs.getInt("playedGames"), rs.getInt("listenedMusics"), rs.getInt("foundMusics") );
+				dto.setId( rs.getString("id") );
+				dtoList.add(dto);
+			}
+			
+			return dtoList;
+		}
+		catch (Exception e) {
+			
+			String message = "Can't find all dto.";
+			LOGGER.error(message, e);
+			throw new EntityManagerException(message, e);
+		}
+	}
+	
+	@Override
+	public ProfilDTO saveOrUpdate(ProfilDTO dto) throws EntityManagerException {
+		
+		Tool.verifyValue("dto", dto);
+
+		ProfilDTO dtoFinded = find(dto);
+		ProfilDTO dtoSaved = (dtoFinded == null) ? save(dto) : update(dtoFinded);
+		
+		return dtoSaved;
+	}
+	
+	
+	
+	private ProfilDTO save(ProfilDTO dto) throws EntityManagerException {
+		
+		try ( Statement statement = em.createStatement() ) {
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("INSERT INTO music(name, avatar, playedGames, listenedMusics, foundMusics) ");
+			sb.append("VALUES ('" + dto.getName() + "', '" + dto.getAvatar() + "', " + dto.getPlayedGames() + ", " + dto.getListenedMusics() + ", " + dto.getFoundMusics() + ")");
+			
+			statement.execute( sb.toString() );
+			
+			ProfilDTO dtoSaved = find(dto);
+			LOGGER.info( "DTO inserted (" + dtoSaved.toString() + ").");
+			
+			return dtoSaved;
+		}
+		catch (Exception e) {
+			
+			String message = "Can't save dto.";
+			LOGGER.error(message, e);
+			throw new EntityManagerException(message, e);
+		}
+	}
+	
+	private ProfilDTO update(ProfilDTO dto) throws EntityManagerException {
+		
+		Tool.verifyValue("dto -> id", dto.getId());
+		
+		try ( Statement statement = em.createStatement() ) {
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("UPDATE music ");
+			sb.append("SET avatar = " + dto.getAvatar() + ", avatar = " + dto.getAvatar() + ", playedGames = " + dto.getPlayedGames() + ", listenedMusics = " + dto.getListenedMusics() + ", foundMusics = " + dto.getFoundMusics() + " ");
+			sb.append("WHERE id = " + dto.getId());
+			
+			statement.execute( sb.toString() );
+			return dto;
+		}
+		catch (Exception e) {
+			
+			String message = "Can't update dto.";
+			LOGGER.error(message, e);
+			throw new EntityManagerException(message, e);
+		}
+	}
+
+}
---
+++ src/test/java/com/myssteriion/blindtest/db/dao/ProfilDAOTest.java
@@ -0,0 +1,168 @@
+package com.myssteriion.blindtest.db.dao;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+
+import org.h2.tools.SimpleResultSet;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.myssteriion.blindtest.AbstractTest;
+import com.myssteriion.blindtest.db.EntityManager;
+import com.myssteriion.blindtest.db.exception.EntityManagerException;
+import com.myssteriion.blindtest.model.dto.ProfilDTO;
+
+
+public class ProfilDAOTest extends AbstractTest {
+
+	@Mock
+	protected EntityManager em;
+	
+	@InjectMocks
+	private ProfilDAO dao;
+	
+	
+	
+	@Test
+	public void find() throws SQLException, EntityManagerException {
+
+		SimpleResultSet rsEmpty = getResultSet();
+		
+		SimpleResultSet rs = getResultSet();
+		rs.addRow("1", "name", "avatar", 1, 2, 3);
+		
+		Statement statement = Mockito.mock(Statement.class);
+		Mockito.when(statement.executeQuery(Mockito.anyString())).thenReturn(rsEmpty, rs);
+		
+		EntityManagerException eme = new EntityManagerException("fake");
+		Mockito.when(em.createStatement()).thenThrow(eme).thenReturn(statement);
+		
+		
+		try {
+			dao.find(null);
+			Assert.fail("Doit lever une IllegalArgumentException car un param est KO.");
+		}
+		catch (IllegalArgumentException e) {
+			verifyException(new IllegalArgumentException("Le champ 'dto' est obligatoire."), e);
+		}
+		
+		
+		ProfilDTO dto = new ProfilDTO("name", "avatar");
+		try {
+			dao.find(dto);
+			Assert.fail("Doit lever une EntityManagerException car le mock throw.");
+		}
+		catch (EntityManagerException e) {
+			verifyException(new EntityManagerException("fake"), eme);
+		}
+		
+		ProfilDTO emDto = dao.find(dto);
+		Assert.assertNull(emDto);
+		
+		dto = new ProfilDTO("name", "avatar");
+		emDto = dao.find(dto);
+		Assert.assertNotNull(emDto);
+	}
+	
+	@Test
+	public void findAll() throws EntityManagerException, SQLException {
+
+		SimpleResultSet rs = getResultSet();
+		rs.addRow("1", "name", "avatar", 1, 2, 3);
+		
+		Statement statement = Mockito.mock(Statement.class);
+		Mockito.when(statement.executeQuery(Mockito.anyString())).thenReturn(rs);
+		
+		EntityManagerException eme = new EntityManagerException("fake");
+		Mockito.when(em.createStatement()).thenThrow(eme).thenReturn(statement);
+		
+		
+		try {
+			dao.findAll();
+			Assert.fail("Doit lever une EntityManagerException car le mock throw.");
+		}
+		catch (EntityManagerException e) {
+			verifyException(new EntityManagerException("fake"), eme);
+		}
+		
+		ProfilDTO dto = dao.findAll().get(0);
+		Assert.assertEquals( "1", dto.getId() );
+		Assert.assertEquals( "name", dto.getName() );
+		Assert.assertEquals( "avatar", dto.getAvatar() );
+		Assert.assertEquals( 1, dto.getPlayedGames() );
+		Assert.assertEquals( 2, dto.getListenedMusics() );
+		Assert.assertEquals( 3, dto.getFoundMusics() );
+	}
+
+	@Test
+	public void saveOrUpdate() throws EntityManagerException, SQLException {
+
+		dao = Mockito.spy( new ProfilDAO() );
+		MockitoAnnotations.initMocks(this);
+		
+		ProfilDTO dto = new ProfilDTO("name", "avatar");
+		dto.setId("1");
+		Mockito.doReturn(null).doReturn(null).doReturn(dto).when(dao).find(Mockito.any(ProfilDTO.class));
+		
+		Statement statement = Mockito.mock(Statement.class);
+		Mockito.when(statement.execute(Mockito.anyString())).thenReturn(true);
+		
+		EntityManagerException eme = new EntityManagerException("fake");
+		Mockito.when(em.createStatement()).thenThrow(eme).thenReturn(statement).thenThrow(eme).thenReturn(statement);
+		
+		
+		try {
+			dao.saveOrUpdate(null);
+			Assert.fail("Doit lever une IllegalArgumentException car un param est KO.");
+		}
+		catch (IllegalArgumentException e) {
+			verifyException(new IllegalArgumentException("Le champ 'dto' est obligatoire."), e);
+		}
+		
+		
+		ProfilDTO dtoToSave = new ProfilDTO("name", "avatar");
+		try {
+			dao.saveOrUpdate(dtoToSave);
+			Assert.fail("Doit lever une EntityManagerException car le mock throw.");
+		}
+		catch (EntityManagerException e) {
+			verifyException(new EntityManagerException("Can't save dto.", eme), e);
+		}
+		
+		ProfilDTO dtoReturned = dao.saveOrUpdate(dtoToSave);
+		Assert.assertEquals(dtoReturned.getId(), "1");
+		
+		
+		dtoToSave.setId("1");
+		try {
+			dao.saveOrUpdate(dtoToSave);
+			Assert.fail("Doit lever une EntityManagerException car le mock throw.");
+		}
+		catch (EntityManagerException e) {
+			verifyException(new EntityManagerException("Can't update dto.", eme), e);
+		}
+		
+		dtoReturned = dao.saveOrUpdate(dtoToSave);
+		Assert.assertEquals(dtoReturned.getId(), "1");
+	}
+	
+	
+	
+	private SimpleResultSet getResultSet() {
+		
+		SimpleResultSet rs = new SimpleResultSet();
+		rs.addColumn("id", Types.BIGINT, 100, 0);
+		rs.addColumn("name", Types.VARCHAR, 255, 0);
+		rs.addColumn("avatar", Types.VARCHAR, 255, 0);
+		rs.addColumn("playedGames", Types.INTEGER, 100, 0);
+		rs.addColumn("listenedMusics", Types.INTEGER, 100, 0);
+		rs.addColumn("foundMusics", Types.INTEGER, 100, 0);
+		return rs;
+	}
+	
+}
