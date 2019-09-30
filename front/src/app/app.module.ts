074af986c140cc1bdc5fa9846317a20d06bf0067
Myssteriion <myssteriion@hotmail.com>
2019-09-30 22:36:06 +0200

    feat(ngModel) : import ngModel

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -22,7 +23,8 @@ import { ProfileResource, UrlInterceptor } from './resources/profile.resource';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     ProfileResource,
