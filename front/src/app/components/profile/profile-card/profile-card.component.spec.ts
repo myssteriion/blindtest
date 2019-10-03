aa3a5dccb0a6e74872ec4dc0bd8de69bb831f407
Myssteriion <myssteriion@hotmail.com>
2019-10-03 20:44:01 +0200

    feat(card) : indent + update

--- front/src/app/components/profile/profile-card/profile-card.component.spec.ts
+++ front/src/app/components/profile/profile-card/profile-card.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfileCardComponent } from './profile-card.component';
 
 describe('ProfileCardComponent', () => {
-  let component: ProfileCardComponent;
-  let fixture: ComponentFixture<ProfileCardComponent>;
+	let component: ProfileCardComponent;
+	let fixture: ComponentFixture<ProfileCardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProfileCardComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ProfileCardComponent]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProfileCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ProfileCardComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- front/src/app/components/profile/profile-card/profile-card.component.ts
+++ front/src/app/components/profile/profile-card/profile-card.component.ts
@@ -1,60 +1,68 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Profile} from 'src/app/interfaces/profile.interface';
-import {AvatarResource} from 'src/app/resources/avatar.resource';
-import {faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {ProfileEditComponent} from 'src/app/components/profile/profile-edit/profile-edit.component';
-import {ToolsService} from "../../../tools/tools.service";
+import { Component, Input, OnInit } from '@angular/core';
+import { Profile } from 'src/app/interfaces/profile.interface';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ProfileEditComponent } from 'src/app/components/profile/profile-edit/profile-edit.component';
+import { ToolsService } from "../../../tools/tools.service";
+import { ProfileResource } from 'src/app/resources/profile.resource';
 
 @Component({
-  selector: 'profile-card',
-  templateUrl: './profile-card.component.html',
-  styleUrls: ['./profile-card.component.css']
+    selector: 'profile-card',
+    templateUrl: './profile-card.component.html',
+    styleUrls: ['./profile-card.component.css']
 })
 export class ProfileCardComponent implements OnInit {
 
-  @Input() profile: Profile;
+    @Input() profile: Profile;
 
-  @Input() canUpdate: boolean;
+    @Input() canUpdate: boolean;
 
-  faEdit = faEdit;
-  
-  faTrashAlt = faTrashAlt;
+    faEdit = faEdit;
 
-
-
-  constructor(private _avatarResource: AvatarResource,
-              private _modalService: NgbModal,
-              private _toolsService: ToolsService) {
-
-  }
-
-  ngOnInit() {
-
-    this._toolsService.verifyValue("profile", this.profile);
-    this._toolsService.verifyValue("profile.avatar", this.profile.avatar);
-    this._toolsService.verifyValue("canUpdate", this.canUpdate);
-  }
+    faTrashAlt = faTrashAlt;
 
 
 
-  public getAvatarFluxForImg() {
-    return this._toolsService.getAvatarFluxForImg(this.profile.avatar);
-  }
+    constructor(private _ngbModal: NgbModal,
+        private _toolsService: ToolsService,
+        private _profileResource: ProfileResource) {
 
-  public edit() {
+    }
 
-    const modalRef = this._modalService.open(ProfileEditComponent, 
-      {
-        
-      }
-    );
-    modalRef.componentInstance.profile = this.profile;
-    modalRef.componentInstance.create = false;
-  }
+    ngOnInit() {
 
-  public delete() {
-    //todo
-  }
+        this._toolsService.verifyValue("profile", this.profile);
+        this._toolsService.verifyValue("profile.avatar", this.profile.avatar);
+        this._toolsService.verifyValue("canUpdate", this.canUpdate);
+    }
+
+
+
+    public getAvatarFluxForImg() {
+        return this._toolsService.getAvatarFluxForImg(this.profile.avatar);
+    }
+
+    public edit() {
+
+        const modalRef = this._ngbModal.open(ProfileEditComponent,
+            {
+                backdrop: 'static'
+            }
+        );
+        modalRef.componentInstance.profile = this.profile;
+        modalRef.componentInstance.create = false;
+
+        modalRef.result
+            .then((result) => {
+                this._profileResource.update(result).subscribe(
+                    response => {
+                        this.profile = response;
+                    },
+                    error => {
+                        new Error("can't update profile : " + error);
+                    }
+                );
+            });
+    }
 
 }
