51dff68ab94ab74ccf728a3aee641af105cb2af9
Myssteriion <myssteriion@hotmail.com>
2019-10-09 21:25:56 +0200

    refactor(indent) : add indent

--- front/src/app/components/profile/profile-view/profile-view.component.ts
+++ front/src/app/components/profile/profile-view/profile-view.component.ts
@@ -3,26 +3,26 @@ import { Profile } from 'src/app/interfaces/profile.interface';
 import { ProfileResource } from 'src/app/resources/profile.resource';
 
 @Component({
-  selector: 'profile-view',
-  templateUrl: './profile-view.component.html',
-  styleUrls: ['./profile-view.component.css']
+    selector: 'profile-view',
+    templateUrl: './profile-view.component.html',
+    styleUrls: ['./profile-view.component.css']
 })
 export class ProfileViewComponent implements OnInit {
 
-  public profiles: Profile[] = [];
-  
-
-
-  constructor(private _profileResource: ProfileResource ) {}
+    public profiles: Profile[] = [];
 
 
 
-  ngOnInit() {
-   
-    this._profileResource.findAll().subscribe(
-      response => { this.profiles = response.items; },
-      error => { console.log("can't find all profiles", error); }
-    );
-  }
+    constructor(private _profileResource: ProfileResource ) {}
+
+
+
+    ngOnInit() {
+
+        this._profileResource.findAllByNameStartingWith("", 0).subscribe(
+            response => { this.profiles = response.content; },
+            error => { console.log("can't find all profiles", error); }
+        );
+    }
 
 }
