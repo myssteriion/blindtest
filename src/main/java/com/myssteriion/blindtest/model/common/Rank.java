243448418f12c5a8159d0620f83c3bbb4b4ad08b
myssteriion <myssteriion@hotmail.com>
2019-10-29 18:11:41 +0100

    feat(rank) : add next attriute

--- src/main/java/com/myssteriion/blindtest/model/common/Rank.java
+++ src/main/java/com/myssteriion/blindtest/model/common/Rank.java
@@ -5,6 +5,30 @@ package com.myssteriion.blindtest.model.common;
  */
 public enum Rank {
 
-	UN_RANKED, FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH, EIGHTH, NINTH, TENTH, ELEVENTH, TWELFTH
+	UN_RANKED(null),
+	TWELFTH(null),
+	ELEVENTH(TWELFTH),
+	TENTH(ELEVENTH),
+	NINTH(TENTH),
+	EIGHTH(NINTH),
+	SEVENTH(EIGHTH),
+	SIXTH(SEVENTH),
+	FIFTH(SIXTH),
+	FOURTH(FIFTH),
+	THIRD(FOURTH),
+	SECOND(THIRD),
+	FIRST(SECOND);
+
+
+
+	private Rank next;
+
+	Rank(Rank next) {
+		this.next = next;
+	}
+
+	public Rank getNext() {
+		return next;
+	}
 
 }
