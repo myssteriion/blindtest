fe687fffeb7a528a8d688e06ebe2573cc1c2f259
Myssteriion <myssteriion@hotmail.com>
2019-12-10 19:20:29 +0100

    feat(rank animation) : remove leave

--- front/src/app/tools/constant.ts
+++ front/src/app/tools/constant.ts
@@ -107,10 +107,6 @@ export const RANK_ICON_ANIMATION =
         transition(':enter', [
             style({ opacity: 0 }),
             animate('1s', style({ opacity: 1 })),
-        ]),
-        transition(':leave', [
-            style({ opacity: 1 }),
-            animate('1s', style({ opacity: 0 })),
         ])
     ]);
 
