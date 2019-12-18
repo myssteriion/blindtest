37631a24b9e8a860cc335f0d8d70c875cbfc1232
Anaïs Ortillon <anais.ortillon@acelys.fr>
2019-12-18 15:37:21 +0100

    feat(statistics) : add translations and constants

--- front/src/app/tools/constant.ts
+++ front/src/app/tools/constant.ts
@@ -196,6 +196,11 @@ export const HORIZONTAL_STACKED_BAR_GRAPH_SIZE = [500, 285];
 export const NUMBER_CARD_GRAPH_SIZE = [500, 125];
 
 /**
+ * Statistics graph size for number card
+ */
+export const NUMBER_CARD_GRAPH_SIZE_SMALL = [700, 125];
+
+/**
  * Statistics graph size for horizontal bar
  */
 export const HORIZONTAL_BAR_GRAPH_SIZE = [700, 400];
