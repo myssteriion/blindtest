c8f4e4ac1af9ecf69df02df664aca1d0f4360016
Anaïs Ortillon <anais.ortillon@acelys.fr>
2019-12-18 15:44:53 +0100

    feat(statistics) : add cards on theme comparison

--- front/src/app/statistics/common/theme-comparison/theme-comparison-view.component.ts
+++ front/src/app/statistics/common/theme-comparison/theme-comparison-view.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, Input} from '@angular/core';
-import {COLOR_SCHEME, SLIDE_ANIMATION} from '../../../tools/constant';
+import {COLOR_SCHEME, NUMBER_CARD_GRAPH_SIZE_SMALL, SLIDE_ANIMATION} from '../../../tools/constant';
+import {ToolsService} from "../../../tools/tools.service";
+import {TranslateService} from "@ngx-translate/core";
 
 /**
  * The theme comparison view.
@@ -19,13 +21,15 @@ export class ThemeComparisonViewComponent implements OnInit {
     public availableThemes: Theme[] = null;
     public selectedTheme: Theme = null;
     public colorScheme = COLOR_SCHEME;
+    public cardResults = [];
+    public view = NUMBER_CARD_GRAPH_SIZE_SMALL;
 
-    constructor() {
+    constructor(private _translate: TranslateService) {
     }
 
     ngOnInit() {
         this.availableThemes = this.gameStatistics.themes;
-        this.selectedTheme = this.availableThemes[0];
+        this.selectTheme(this.availableThemes[0]);
     }
 
     /**
@@ -34,6 +38,26 @@ export class ThemeComparisonViewComponent implements OnInit {
      */
     selectTheme(theme: Theme) {
         this.selectedTheme = theme;
+        this.updateCardsNumber();
+    }
+
+    /**
+     * Update value on card number
+     */
+    updateCardsNumber() {
+        this.cardResults = [];
+        let listenedMusics = 0;
+        this.availableThemes.forEach(theme => {
+            listenedMusics = ToolsService.isNull(this.gameStatistics.listenedMusics[theme]) ? listenedMusics : listenedMusics + this.gameStatistics.listenedMusics[theme];
+        });
+        this.cardResults.push({
+            name: this._translate.instant("STATISTICS.CATEGORIES.TOTAL_LISTENED_MUSICS"),
+            value: listenedMusics
+        });
+        this.cardResults.push({
+            name: this._translate.instant("STATISTICS.CATEGORIES.LISTENED_MUSICS_BY_THEME.NUMBER_LISTENED_MUSICS_IN_THEME"),
+            value: ToolsService.isNull(this.gameStatistics.listenedMusics[this.selectedTheme]) ? 0 : this.gameStatistics.listenedMusics[this.selectedTheme]
+        });
     }
 
     /**
