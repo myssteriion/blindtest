6c0dd4df2d88fe8cc7ab4cabc2355a6defe8f0e8
myssteriion <myssteriion@hotmail.com>
2020-02-21 14:56:49 +0100

    test(spring) : remove useless tag

--- src/test/java/com/myssteriion/blindtest/AbstractTest.java
+++ src/test/java/com/myssteriion/blindtest/AbstractTest.java
@@ -1,15 +1,10 @@
 package com.myssteriion.blindtest;
 
-import com.myssteriion.utils.BeanFactory;
-import com.myssteriion.utils.rest.RestUtils;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-@ComponentScan( basePackages = {"com.myssteriion.blindtest", "com.myssteriion.utils"} )
 public abstract class AbstractTest {
 }
