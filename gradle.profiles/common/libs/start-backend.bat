09df815de5db1aa555cc9e3b4d2bfece9ebf793e
myssteriion <myssteriion@hotmail.com>
2020-02-21 17:51:52 +0100

    chore(version) : update version in bat file

--- gradle.profiles/common/libs/start-backend.bat
+++ gradle.profiles/common/libs/start-backend.bat
@@ -1,5 +1,5 @@
 set SPRING_CONFIG_LOCATION=file:./libs/conf
 set logging=./libs/conf/logback.xml
 
-start "b_blind_test" /MIN java -jar -Dlogging.config=%logging% libs/blindtest-1.0.war
+start "b_blind_test" /MIN java -jar -Dlogging.config=%logging% libs/blindtest-1.1-SNAPSHOT.war
 exit
