1aa96c9e95d093b98fc7b3a27fedd33e555c61dd
Myssteriion <myssteriion@hotmail.com>
2020-02-23 20:21:31 +0100

    feat(migration) : update query

--- migration/bdd/1.0-to-1.1.sql
+++ migration/bdd/1.0-to-1.1.sql
@@ -4,10 +4,10 @@
 DELETE FROM music WHERE theme = 8 OR theme = 9;
 
 -- select impacted profile_stat
-SELECT id, found_musics FROM profile_stat where found_musics like '%JEUX%' ORDER BY id ASC;
-SELECT id, found_musics FROM profile_stat where found_musics like '%CLASSIQUE%' ORDER BY id ASC;
-SELECT id, listened_musics FROM profile_stat where listened_musics like '%JEUX%' ORDER BY id ASC;
-SELECT id, listened_musics FROM profile_stat where listened_musics like '%CLASSIQUE%' ORDER BY id ASC;
+SELECT found_musics, id FROM profile_stat where found_musics like '%JEUX%' ORDER BY id ASC;
+SELECT found_musics, id FROM profile_stat where found_musics like '%CLASSIQUE%' ORDER BY id ASC;
+SELECT listened_musics, id FROM profile_stat where listened_musics like '%JEUX%' ORDER BY id ASC;
+SELECT listened_musics, id FROM profile_stat where listened_musics like '%CLASSIQUE%' ORDER BY id ASC;
 
 -- update impacted profile_stat
 UPDATE profile_stat SET found_musics = '' WHERE id = -1 ORDER BY id ASC;
