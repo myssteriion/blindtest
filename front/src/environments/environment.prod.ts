c62201883e28ca921535a8ab232acf488e4c3aa9
myssteriion <myssteriion@hotmail.com>
2020-03-04 14:18:07 +0100

    refactor(syntax) : indent all files

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
-	production: true,
-	version: "1.1.0-SNAPSHOT",
-	baseBackendUrl: "http://localhost:8085/musics-blindtest",
-
-	maxPlayers: 16,
-	minPlayers: 2,
-	itemPerPageAvatars: 24,
-	itemPerPageProfiles: 15,
-
-	nbLoseMax: 4,
-	nbMusicsMinLabelHelp: 100
+    production: true,
+    version: "1.1.0-SNAPSHOT",
+    baseBackendUrl: "http://localhost:8085/musics-blindtest",
+    
+    maxPlayers: 16,
+    minPlayers: 2,
+    itemPerPageAvatars: 24,
+    itemPerPageProfiles: 15,
+    
+    nbLoseMax: 4,
+    nbMusicsMinLabelHelp: 100
 };
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -3,17 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	production: false,
-	version: "1.1.0-SNAPSHOT",
-	baseBackendUrl: "http://localhost:8085/musics-blindtest",
-
-	maxPlayers: 16,
-	minPlayers: 2,
-	itemPerPageAvatars: 24,
-	itemPerPageProfiles: 15,
-
-	nbLoseMax: 4,
-	nbMusicsMinLabelHelp: 100
+    production: false,
+    version: "1.1.0-SNAPSHOT",
+    baseBackendUrl: "http://localhost:8085/musics-blindtest",
+    
+    maxPlayers: 16,
+    minPlayers: 2,
+    itemPerPageAvatars: 24,
+    itemPerPageProfiles: 15,
+    
+    nbLoseMax: 4,
+    nbMusicsMinLabelHelp: 100
 };
 
 /*
