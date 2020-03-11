03abdd13b68a2091de3494134363830ca5ad9c10
myssteriion <myssteriion@hotmail.com>
2020-03-11 12:30:37 +0100

    refactor(syntax) : use double quote

--- front/src/app/resources/avatar.resource.ts
+++ front/src/app/resources/avatar.resource.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http'
-import {Observable} from 'rxjs';
-import {Avatar} from 'src/app/interfaces/dto/avatar.interface';
-import {Page} from 'src/app/interfaces/base/page.interface';
-import {environment} from 'src/environments/environment';
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpParams} from "@angular/common/http"
+import {Observable} from "rxjs";
+import {Avatar} from "src/app/interfaces/dto/avatar.interface";
+import {Page} from "src/app/interfaces/base/page.interface";
+import {environment} from "src/environments/environment";
 
 /**
  * Avatar resource.
@@ -31,9 +31,9 @@ export class AvatarResource {
 	public findAllByNameStartingWith(prefixName: string, pageNumber: number): Observable< Page<Avatar> > {
 		
 		let params = new HttpParams();
-		params = params.set('prefixName', prefixName);
-		params = params.set('pageNumber', pageNumber.toString());
-		params = params.set('itemPerPage', environment.itemPerPageAvatars.toString());
+		params = params.set("prefixName", prefixName);
+		params = params.set("pageNumber", pageNumber.toString());
+		params = params.set("itemPerPage", environment.itemPerPageAvatars.toString());
 		
 		return this._http.get< Page<Avatar> >( this.path, { params: params } );
 	}
--- front/src/app/resources/profile.resource.ts
+++ front/src/app/resources/profile.resource.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http'
-import {Observable} from 'rxjs';
-import {Profile} from '../interfaces/dto/profile.interface';
-import {Page} from '../interfaces/base/page.interface';
-import {environment} from 'src/environments/environment';
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpParams} from "@angular/common/http"
+import {Observable} from "rxjs";
+import {Profile} from "../interfaces/dto/profile.interface";
+import {Page} from "../interfaces/base/page.interface";
+import {environment} from "src/environments/environment";
 
 /**
  * Profile resource.
@@ -49,9 +49,9 @@ export class ProfileResource {
 	public findAllByNameStartingWith(prefixName: string, pageNumber: number): Observable< Page<Profile> > {
 		
 		let params = new HttpParams();
-		params = params.set('prefixName', prefixName);
-		params = params.set('pageNumber', pageNumber.toString());
-		params = params.set('itemPerPage', environment.itemPerPageProfiles.toString());
+		params = params.set("prefixName", prefixName);
+		params = params.set("pageNumber", pageNumber.toString());
+		params = params.set("itemPerPage", environment.itemPerPageProfiles.toString());
 		
 		return this._http.get< Page<Profile> >( this.path, { params: params } );
 	}
