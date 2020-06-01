65c00223f5ead8dda6816576db93eb588fa6f1f2
Myssteriion <myssteriion@hotmail.com>
2020-06-01 14:20:29 +0200

    feat(controller) : remove CrossOrigin tag

--- src/main/java/com/myssteriion/blindtest/controller/GameController.java
+++ src/main/java/com/myssteriion/blindtest/controller/GameController.java
@@ -13,7 +13,6 @@ import com.myssteriion.utils.rest.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +24,6 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Controller for GameDTO.
  */
-@CrossOrigin( origins = {"http://localhost:8085"} )
 @RestController
 @RequestMapping(path = "games")
 public class GameController {
