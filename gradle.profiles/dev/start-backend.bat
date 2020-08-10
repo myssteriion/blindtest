c41d5fda75de4c53923ecdf6e8142f80172cc258
Myssteriion <myssteriion@hotmail.com>
2020-08-10 15:28:49 +0200

    chore(version) : set 3.0.0-SNAPSHOT

--- gradle.profiles/dev/start-backend.bat
+++ gradle.profiles/dev/start-backend.bat
@@ -1,5 +1,5 @@
 set SPRING_CONFIG_LOCATION=file:./libs/conf
 set logging=./libs/conf/logback.xml
 
-java -jar -Dlogging.config=%logging% libs\musics-blindtest-2.1.0-SNAPSHOT.war
+java -jar -Dlogging.config=%logging% libs\musics-blindtest-3.0.0-SNAPSHOT.war
 pause
