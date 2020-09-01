4e78f7a50fe574cb7661d449261e82a27091c6cd
Myssteriion <myssteriion@hotmail.com>
2020-09-01 19:51:01 +0200

    feat(theme info) : remove equals and hashCode

--- backend/src/main/java/com/myssteriion/blindtest/model/music/ThemeInfo.java
+++ backend/src/main/java/com/myssteriion/blindtest/model/music/ThemeInfo.java
@@ -3,8 +3,6 @@ package com.myssteriion.blindtest.model.music;
 import com.myssteriion.blindtest.model.common.Theme;
 import com.myssteriion.utils.model.IModel;
 
-import java.util.Objects;
-
 /**
  * Theme info (nb musics by ConnectionMode).
  */
@@ -82,24 +80,6 @@ public class ThemeInfo implements IModel {
         return this;
     }
     
-    
-    @Override
-    public boolean equals(Object o) {
-        
-        if (this == o)
-            return true;
-        
-        if (o == null || getClass() != o.getClass())
-            return false;
-        
-        ThemeInfo themeInfo = (ThemeInfo) o;
-        return theme == themeInfo.theme && Objects.equals(nbMusics, themeInfo.nbMusics);
-    }
-    
-    @Override
-    public int hashCode() {
-        return Objects.hash(theme, nbMusics);
-    }
     
     @Override
     public String toString() {
